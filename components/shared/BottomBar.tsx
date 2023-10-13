a300b29bc8e6dd11a5f8195772889f33b42ce125
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-10-12 21:26:50 -0300

    added the logout funcionality in the bottombar

--- components/shared/BottomBar.tsx
+++ components/shared/BottomBar.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import { siteNavConfigWithSession } from "@/config/site";
+import { signOut } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 function BottomBar() {
   const pathname = usePathname();
+  const router = useRouter();
+
+  const handleSignOut = () => {
+    signOut();
+    router.push("/");
+  };
   return (
     <section className="bottombar">
       <div className="bottombar_container">
@@ -14,11 +21,33 @@ function BottomBar() {
           const isActive =
             (pathname.includes(link.href) && link.href.length > 1) ||
             pathname === link.href;
+          if (link.label === "Sair") {
+            return (
+              <a
+                key={link.label}
+                className="bottombar_link cursor-pointer"
+                onClick={handleSignOut}
+              >
+                <Image
+                  src={link.imgURL}
+                  alt={link.label}
+                  width={24}
+                  height={24}
+                />
+                <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                  {link.label.split(/\s+/)[0]}
+                </p>
+              </a>
+            );
+          }
+
           return (
             <Link
               href={link.href}
               key={link.label}
-              className={`bottombar_link ${isActive && "bg-[#d627d0] bg-opacity-50"}`}
+              className={`bottombar_link ${
+                isActive && "bg-[#d627d0] bg-opacity-50"
+              }`}
             >
               <Image
                 src={link.imgURL}
