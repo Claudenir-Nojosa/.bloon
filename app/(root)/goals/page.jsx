27d39a8a3a9a4d7953b4b747ef841f1099dc97da
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-10-21 14:40:59 -0300

    added month filter in the goals page

--- app/(root)/goals/page.jsx
+++ app/(root)/goals/page.jsx
@@ -12,7 +12,7 @@ import {
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Image from "next/image";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -27,8 +27,15 @@ import { Label } from "@/components/ui/label";
 import { ExpenseLimitForm } from "@/components/forms/expenseLimit";
 import numeral from "numeral";
 import { Progress } from "@nextui-org/react";
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const Goals = () => {
+  const currentMonth = dayjs().locale("pt-br").format("MMMM");
+  const capitalizedMonth =
+    currentMonth.charAt(0).toUpperCase() + currentMonth.slice(1);
+  const [selectedMonth, setSelectedMonth] = useState(capitalizedMonth);
   // fetch expense tags
   const { data: dataExpenseTags, isLoading: isLoadingExpenseTags } = useQuery({
     queryKey: ["expenseTags"],
@@ -47,6 +54,31 @@ const Goals = () => {
     },
   });
   console.log(dataExpense);
+
+  const combinedData = dataExpense?.expenses?.map((item) => {
+    const dateMonth = dayjs(item.date, "DD/MM/YYYY")
+      .locale("pt-br")
+      .format("MMMM");
+    const capitalizedMonth =
+      dateMonth.charAt(0).toUpperCase() + dateMonth.slice(1);
+    return {
+      ...item,
+      month: capitalizedMonth,
+    };
+  });
+  console.log(combinedData);
+
+  const filterExpensesByMonth = (expenses, month) => {
+    if (expenses) {
+      return expenses.filter((expense) => {
+        const expenseMonth = expense.month;
+        return expenseMonth === month;
+      });
+    } else {
+      // Handle the case where expenses are undefined
+      return [];
+    }
+  };
   const calculateProgressValue = (tagId, expenses) => {
     const paidExpenses = expenses.filter((expense) => expense.paid);
 
@@ -60,11 +92,39 @@ const Goals = () => {
       (total, expense) => total + expense.value,
       0
     );
-
+    console.log(calculateProgressValue);
     const tag = expensesWithSameTag[0].ExpenseTag;
     const monthlyLimit = tag.monthlyLimit || 0;
     const progressValue = (totalExpenseValue / monthlyLimit) * 100;
     return progressValue;
+  };
+  const months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
+  // Funções para navegar entre os meses
+  const goToPreviousMonth = () => {
+    const currentMonthIndex = months.indexOf(selectedMonth || "");
+    if (currentMonthIndex > 0) {
+      setSelectedMonth(months[currentMonthIndex - 1]);
+    }
+  };
+
+  const goToNextMonth = () => {
+    const currentMonthIndex = months.indexOf(selectedMonth || "");
+    if (currentMonthIndex < months.length - 1) {
+      setSelectedMonth(months[currentMonthIndex + 1]);
+    }
   };
 
   return (
@@ -73,22 +133,49 @@ const Goals = () => {
       <p className="text-zinc-400 mb-14">
         Para editar os limites, basta clicar no ícone da categoria
       </p>
+      <div className="min-w-[300px] justify-center mt-4 mb-8 flex text-center items-center">
+        <button className="mr-3 text-5xl" onClick={goToPreviousMonth}>
+          <ChevronLeft />
+        </button>
+        {selectedMonth && (
+          <span className="text-lg text-zinc-400">{selectedMonth}</span>
+        )}
+        <button className="ml-3 text-5xl" onClick={goToNextMonth}>
+          <ChevronRight />
+        </button>
+      </div>
       {dataExpenseTags?.map((tag) => {
-        const tagId = tag.id; // ID da tag específica
-        const progressValue = dataExpense
-          ? calculateProgressValue(tagId, dataExpense.expenses)
-          : 0;
+        const tagId = tag.id;
+
+        // Filtrar as despesas do mês selecionado
+        const expensesForMonth = filterExpensesByMonth(
+          combinedData,
+          selectedMonth
+        );
+
+        // Calcular progresso e falta com base no mês selecionado
+        const progressValue =
+          expensesForMonth.length > 0
+            ? calculateProgressValue(tagId, expensesForMonth)
+            : 0;
+
         const monthlyLimit = tag.monthlyLimit || 0;
-        const totalExpenseValue = progressValue * 0.01 * monthlyLimit;
+        const totalExpenseValue = (progressValue / 100) * monthlyLimit;
         const amountToExpend = monthlyLimit - totalExpenseValue;
+
         const faltaTexto =
           progressValue >= 100
             ? `Você já atingiu o limite e passou por R$ ${numeral(
                 -amountToExpend
               ).format("0.00")} reais`
             : `Falta: R$ ${numeral(amountToExpend).format("0.00")}`;
+        console.log("Selected Month:", selectedMonth);
+        console.log("Combined Data:", combinedData);
+        console.log("Tag ID:", tagId);
+        console.log("Expenses for Month:", expensesForMonth);
         return (
           <div className="flex flex-col mb-10 w-full" key={tag.id}>
+            {/* Renderização do componente com os valores atualizados */}
             <div className="flex items-center font-semibold text-xl">
               <Button variant="ghost">
                 <Dialog>
