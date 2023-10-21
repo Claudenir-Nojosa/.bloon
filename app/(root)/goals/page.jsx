5e67179fec909719aed0bf6464b3a7e19bc27d6d
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-10-21 10:33:52 -0300

    added only the paid expenses in the goals, and added a progressbar

--- app/(root)/goals/page.jsx
+++ app/(root)/goals/page.jsx
@@ -26,7 +26,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ExpenseLimitForm } from "@/components/forms/expenseLimit";
 import numeral from "numeral";
-import { Progress } from "@/components/ui/progress";
+import { Progress } from "@nextui-org/react";
 
 const Goals = () => {
   // fetch expense tags
@@ -48,7 +48,9 @@ const Goals = () => {
   });
   console.log(dataExpense);
   const calculateProgressValue = (tagId, expenses) => {
-    const expensesWithSameTag = expenses.filter(
+    const paidExpenses = expenses.filter((expense) => expense.paid);
+
+    const expensesWithSameTag = paidExpenses.filter(
       (expense) => expense.expenseTagId === tagId
     );
     if (expensesWithSameTag.length === 0) {
@@ -61,8 +63,7 @@ const Goals = () => {
 
     const tag = expensesWithSameTag[0].ExpenseTag;
     const monthlyLimit = tag.monthlyLimit || 0;
-    const progressValue = monthlyLimit - totalExpenseValue;
-
+    const progressValue = (totalExpenseValue / monthlyLimit) * 100;
     return progressValue;
   };
 
@@ -77,7 +78,15 @@ const Goals = () => {
         const progressValue = dataExpense
           ? calculateProgressValue(tagId, dataExpense.expenses)
           : 0;
-
+        const monthlyLimit = tag.monthlyLimit || 0;
+        const totalExpenseValue = progressValue * 0.01 * monthlyLimit;
+        const amountToExpend = monthlyLimit - totalExpenseValue;
+        const faltaTexto =
+          progressValue >= 100
+            ? `Você já atingiu o limite e passou por R$ ${numeral(
+                -amountToExpend
+              ).format("0.00")} reais`
+            : `Falta: R$ ${numeral(amountToExpend).format("0.00")}`;
         return (
           <div className="flex flex-col mb-10 w-full" key={tag.id}>
             <div className="flex items-center font-semibold text-xl">
@@ -104,14 +113,16 @@ const Goals = () => {
                 </Dialog>
               </Button>
               <div className="flex flex-col w-full">
-                <Progress value={progressValue} />
+                <Progress
+                  value={progressValue}
+                  color={`${progressValue >= 100 ? "danger" : "success"}`}
+                  showValueLabel={true}
+                />
                 <span className="mt-2"> {tag.name}</span>
                 <span className="text-zinc-400 text-lg">
                   Limite atual: R$ {numeral(tag.monthlyLimit).format("0.00")}
                 </span>
-                <span className="text-zinc-400 text-lg">
-                  Falta: R$ {numeral(progressValue).format("0.00")}
-                </span>
+                <span className="text-zinc-400 text-lg">{faltaTexto}</span>
               </div>
             </div>
           </div>
--- app/api/expenses/route.js
+++ app/api/expenses/route.js
@@ -14,6 +14,7 @@ export async function GET() {
         description: true,
         value: true,
         date: true,
+        paid: true,
         expenseTagId: true,
         ExpenseTag: true,
         User: true,
